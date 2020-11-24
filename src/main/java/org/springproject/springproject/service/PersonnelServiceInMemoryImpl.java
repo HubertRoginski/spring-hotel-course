b3c850cd2e2dd26b18ddd26e931245dc7953ccd1
Hubert <roginski.h@gmail.com>
2020-11-24 13:18:01 +0100

    Heroku configuration #5

--- src/main/java/org/springproject/springproject/service/PersonnelServiceInMemoryImpl.java
+++ src/main/java/org/springproject/springproject/service/PersonnelServiceInMemoryImpl.java
@@ -18,67 +18,9 @@ import java.util.Map;
 @Service
 @Scope("singleton")
 public class PersonnelServiceInMemoryImpl implements PersonnelService {
-
-    private final Map<Long, Personnel> personnelMap = new HashMap<>();
-    private Long nextId = 1L;
-    private final HotelPersonnelConfig hotelPersonnelConfig;
-
-    @Value("${hotel.personnel.owner.name:Name}")
-    private String ownerName;
-
-    @Value("${hotel.personnel.owner.last-name:lastName}")
-    private String lastName;
-
-    @Value("${hotel.personnel.owner.hire-date:2000-07-04}")
-    private String hireDate;
-
-    @Value("${hotel.personnel.owner.position:position}")
-    private String position;
-
-    @Value("${hotel.personnel.owner.salary:200.0}")
-    private Double salary;
-
-    @Value("${hotel.personnel.owner.sick-leave:false}")
-    private Boolean sickLeave;
-
-    public PersonnelServiceInMemoryImpl(HotelPersonnelConfig hotelPersonnelConfig) {
-        this.hotelPersonnelConfig = hotelPersonnelConfig;
-    }
-
-    @PostConstruct
-    public void init() {
-        personnelMap.put(nextId, Personnel.builder()
-                .id(getNextId())
-                .firstName(ownerName)
-                .lastName(lastName)
-                .hireDate(LocalDate.parse(hireDate))
-                .position(position)
-                .salary(salary)
-                .sickLeave(sickLeave)
-                .build());
-        personnelMap.put(nextId, Personnel.builder()
-                .id(getNextId())
-                .firstName(hotelPersonnelConfig.getNames().get(2))
-                .lastName(lastName)
-                .hireDate(LocalDate.parse(hireDate))
-                .position(position)
-                .salary(salary)
-                .sickLeave(sickLeave)
-                .build());
-        personnelMap.put(nextId, Personnel.builder()
-                .id(getNextId())
-                .firstName(hotelPersonnelConfig.getNames().get(0))
-                .lastName(hotelPersonnelConfig.getPeople().get(hotelPersonnelConfig.getNames().get(0)))
-                .hireDate(LocalDate.parse(hireDate))
-                .position(position)
-                .salary(salary)
-                .sickLeave(sickLeave)
-                .build());
-    }
-
     @Override
     public Personnel getPersonnelById(Long id) {
-        return personnelMap.getOrDefault(id, null);
+        return null;
     }
 
     @Override
@@ -86,35 +28,23 @@ public class PersonnelServiceInMemoryImpl implements PersonnelService {
         return null;
     }
 
-
-
     @Override
     public boolean removePersonnelById(Long id) {
-        if (personnelMap.containsKey(id)){
-            personnelMap.remove(id);
-            return true;
-        }
         return false;
     }
 
     @Override
     public Personnel createNewPersonnel(Personnel personnel) {
-        personnel.setId(getNextId());
-        personnelMap.put(personnel.getId(), personnel);
-        return personnel;
+        return null;
     }
 
     @Override
     public List<Personnel> createBatchOfPersonnel(List<Personnel> personnels) {
-        return addPersonnel(personnels);
+        return null;
     }
 
     @Override
     public Personnel updatePersonnelById(Long id, Personnel personnel) {
-        if (personnelMap.containsKey(id)){
-            personnel.setId(id);
-            return personnelMap.replace(id, personnel);
-        }
         return null;
     }
 
@@ -133,16 +63,130 @@ public class PersonnelServiceInMemoryImpl implements PersonnelService {
 
     }
 
-    private List<Personnel> addPersonnel(List<Personnel> personnels){
-        personnels.forEach(personnel -> {
-            personnel.setId(getNextId());
-            personnelMap.put(personnel.getId(), personnel);
-        });
-        return personnels;
-
-    }
-
-    private Long getNextId(){
-        return nextId++;
-    }
+//    private final Map<Long, Personnel> personnelMap = new HashMap<>();
+//    private Long nextId = 1L;
+//    private final HotelPersonnelConfig hotelPersonnelConfig;
+//
+//    @Value("${hotel.personnel.owner.name:Name}")
+//    private String ownerName;
+//
+//    @Value("${hotel.personnel.owner.last-name:lastName}")
+//    private String lastName;
+//
+//    @Value("${hotel.personnel.owner.hire-date:2000-07-04}")
+//    private String hireDate;
+//
+//    @Value("${hotel.personnel.owner.position:position}")
+//    private String position;
+//
+//    @Value("${hotel.personnel.owner.salary:200.0}")
+//    private Double salary;
+//
+//    @Value("${hotel.personnel.owner.sick-leave:false}")
+//    private Boolean sickLeave;
+//
+//    public PersonnelServiceInMemoryImpl(HotelPersonnelConfig hotelPersonnelConfig) {
+//        this.hotelPersonnelConfig = hotelPersonnelConfig;
+//    }
+//
+//    @PostConstruct
+//    public void init() {
+//        personnelMap.put(nextId, Personnel.builder()
+//                .id(getNextId())
+//                .firstName(ownerName)
+//                .lastName(lastName)
+//                .hireDate(LocalDate.parse(hireDate))
+//                .position(position)
+//                .salary(salary)
+//                .sickLeave(sickLeave)
+//                .build());
+//        personnelMap.put(nextId, Personnel.builder()
+//                .id(getNextId())
+//                .firstName(hotelPersonnelConfig.getNames().get(2))
+//                .lastName(lastName)
+//                .hireDate(LocalDate.parse(hireDate))
+//                .position(position)
+//                .salary(salary)
+//                .sickLeave(sickLeave)
+//                .build());
+//        personnelMap.put(nextId, Personnel.builder()
+//                .id(getNextId())
+//                .firstName(hotelPersonnelConfig.getNames().get(0))
+//                .lastName(hotelPersonnelConfig.getPeople().get(hotelPersonnelConfig.getNames().get(0)))
+//                .hireDate(LocalDate.parse(hireDate))
+//                .position(position)
+//                .salary(salary)
+//                .sickLeave(sickLeave)
+//                .build());
+//    }
+//
+//    @Override
+//    public Personnel getPersonnelById(Long id) {
+//        return personnelMap.getOrDefault(id, null);
+//    }
+//
+//    @Override
+//    public List<Personnel> getAllPersonnel(Integer page, Integer size) {
+//        return null;
+//    }
+//
+//
+//
+//    @Override
+//    public boolean removePersonnelById(Long id) {
+//        if (personnelMap.containsKey(id)){
+//            personnelMap.remove(id);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public Personnel createNewPersonnel(Personnel personnel) {
+//        personnel.setId(getNextId());
+//        personnelMap.put(personnel.getId(), personnel);
+//        return personnel;
+//    }
+//
+//    @Override
+//    public List<Personnel> createBatchOfPersonnel(List<Personnel> personnels) {
+//        return addPersonnel(personnels);
+//    }
+//
+//    @Override
+//    public Personnel updatePersonnelById(Long id, Personnel personnel) {
+//        if (personnelMap.containsKey(id)){
+//            personnel.setId(id);
+//            return personnelMap.replace(id, personnel);
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Personnel> getPersonnelBySickLeave(Boolean sickLeave) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Personnel> getPersonnelByPosition(String position) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void cureAllPersonnel() {
+//
+//    }
+//
+//    private List<Personnel> addPersonnel(List<Personnel> personnels){
+//        personnels.forEach(personnel -> {
+//            personnel.setId(getNextId());
+//            personnelMap.put(personnel.getId(), personnel);
+//        });
+//        return personnels;
+//
+//    }
+//
+//    private Long getNextId(){
+//        return nextId++;
+//    }
 }
